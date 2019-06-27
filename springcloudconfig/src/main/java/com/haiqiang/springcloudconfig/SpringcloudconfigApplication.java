727dbf0f2428e9d1ec3e1268432f91cd4a50c6c9
chenhaiqiang <1208105440@qq.com>
2019-06-27 16:38:07 +0800

    20190627springcloud实例

---
+++ springcloudconfig/src/main/java/com/haiqiang/springcloudconfig/SpringcloudconfigApplication.java
@@ -0,0 +1,13 @@
+package com.haiqiang.springcloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudconfigApplication.class, args);
+    }
+
+}
